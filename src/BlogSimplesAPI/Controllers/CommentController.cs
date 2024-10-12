1605e618afe9be42ac1bc2009cc3f238db9d7161
Viliane <vilianeso@gmail.com>
2024-10-12 18:10:23 -0300

    Role Admin

--- src/BlogSimplesAPI/Controllers/CommentController.cs
+++ src/BlogSimplesAPI/Controllers/CommentController.cs
@@ -18,10 +18,10 @@ namespace BlogSimplesAPI.Controllers
             _logger = logger;
             _commentServices = commentServices;
         }
-
-        [AllowAnonymous]
+                
         [HttpGet()]
         [ProducesResponseType(typeof(Comments), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public IActionResult GetAll()
         {
             if (!ModelState.IsValid) { return ValidationProblem(ModelState); }
@@ -31,6 +31,7 @@ namespace BlogSimplesAPI.Controllers
 
         [HttpGet("id")]
         [ProducesResponseType(typeof(Comments), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Get(int id)
         {
@@ -42,6 +43,7 @@ namespace BlogSimplesAPI.Controllers
 
         [HttpPost()]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Insert(CommentView commentsView)
         {
@@ -58,7 +60,10 @@ namespace BlogSimplesAPI.Controllers
         }
 
         [HttpPut("id")]
+        [Authorize(Roles = "Admin")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Put(int id, CommentView commentsView)
         {
@@ -77,7 +82,10 @@ namespace BlogSimplesAPI.Controllers
         }
 
         [HttpDelete("id")]
+        [Authorize(Roles = "Admin")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Delete(int id)
         {
--- src/BlogSimplesAPI/Controllers/PostController.cs
+++ src/BlogSimplesAPI/Controllers/PostController.cs
@@ -32,6 +32,7 @@ namespace BlogSimplesAPI.Controllers
 
         [HttpGet("id")]
         [ProducesResponseType(typeof(Post), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Get(int id)
         {
@@ -42,6 +43,7 @@ namespace BlogSimplesAPI.Controllers
 
         [HttpPost()]
         [ProducesResponseType(typeof(Post), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Post(PostView postView)
         {
@@ -60,8 +62,11 @@ namespace BlogSimplesAPI.Controllers
         }
 
         [HttpPut("id")]
+        [Authorize(Roles = "Admin")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]        
         public IActionResult Put(int id, PostView postView)
         {
             if (!ModelState.IsValid) { return ValidationProblem(ModelState); }
@@ -81,7 +86,10 @@ namespace BlogSimplesAPI.Controllers
         }
 
         [HttpDelete("id")]
+        [Authorize(Roles = "Admin")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Delete(int id)
         {
